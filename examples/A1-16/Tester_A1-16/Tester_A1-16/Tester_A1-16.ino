272ffe47b3a7790f324150295f2831597dcec2e3
ZachariasHultman <49194739+ZachariasHultman@users.noreply.github.com>
2019-04-04 16:27:31 +0200

    Update Tester_A1-16.ino
    Ytterligare fix. Gjort A1-16 i par om 3 istället

--- examples/A1-16/Tester_A1-16/Tester_A1-16/Tester_A1-16.ino
+++ examples/A1-16/Tester_A1-16/Tester_A1-16/Tester_A1-16.ino
@@ -2,6 +2,7 @@
 #include <XL320.h>
 #include <XYZrobotServo.h>
 #include <SoftwareSerial.h>
+//#include <Servo.h>
 
 #define SERVO_COUNT 6
 
@@ -13,6 +14,8 @@
 
 #define rxPin10 10
 #define txPin11 11
+
+#define txPin12 12
 
 SoftwareSerial A116servoSerial=SoftwareSerial(rxPin0, txPin1);
 
@@ -28,15 +31,17 @@ XYZrobotServo servo2(A116servoSerial,2);
 
 //XYZrobotServo servo3(A116servoSerial,3);
 
-XYZrobotServo servo4(A116servo2Serial,4);
+XYZrobotServo servo4(A116servoSerial,4);
 
 XYZrobotServo servo5(A116servo2Serial,5);
 
-XYZrobotServo servo6(A116servoSerial,6);
+XYZrobotServo servo6(A116servo2Serial,6);
 
-XYZrobotServo servo7(A116servoSerial,7);
+XYZrobotServo servo7(A116servo2Serial,7);
 
 XL320 servoFinger;
+
+//Servo Elbow;
 
 
 
@@ -57,11 +62,14 @@ void setup(){
   pinMode(rxPin10,INPUT);
   pinMode(txPin11,OUTPUT);
 
+  pinMode(txPin12,OUTPUT);
+
 
    A116servoSerial.begin(115200);
    A116servo2Serial.begin(115200);
    XL320servoSerial.begin(115200);
    servoFinger.begin(XL320servoSerial);
+   //Elbow.attach(txPin12);
 
 
 }
@@ -72,63 +80,37 @@ void loop() {
       int playtime =75;
       int distance = 10000;
 
-/*
-  servo1.setPosition(distance,playtime);
-  delay(2000);
-
-  servo1.setPosition(0,playtime);
-  delay(1000);
-*/
 int i;
+
 
   for(i=1;i<=1000;i++){
 
     
     servo1.setPosition(i,10);
     servo2.setPosition(i,10);
-
     servo4.setPosition(i,10);
+    
     servo5.setPosition(i,10);
+    servo6.setPosition(i,10);
+    servo7.setPosition(i,10);
 
-    servoFinger.moveJoint(2,i); 
+    servoFinger.moveJoint(2,i);
+
+    //Elbow.write(j);
+     
     
   }
+
+  
   servo1.setPosition(0,10);
   servo2.setPosition(0,10);
   servo4.setPosition(0,10);
   servo5.setPosition(0,10);
-  servoFinger.moveJoint(2,0); 
+  servo6.setPosition(0,10);
+  servo7.setPosition(0,10);
+  servoFinger.moveJoint(2,0);
+  //Elbow.writeMicroseconds(1500);
 
-/*
-  servo2.setPosition(distance,playtime);
-  delay(2000);
 
-  servo2.setPosition(0,playtime);
-  delay(1000);
-
-  servo4.setPosition(distance,playtime);
-  delay(2000);
-
-  servo4.setPosition(0,playtime);
-  delay(1000);
-
-  servo5.setPosition(distance,playtime);
-  delay(2000);
-
-  servo5.setPosition(0,playtime);
-  delay(1000);
-
-  servo6.setPosition(distance,playtime);
-  delay(2000);
-
-  servo6.setPosition(0,playtime);
-  delay(1000);
-
-  servo7.setPosition(distance,playtime);
-  delay(2000);
-
-  servo7.setPosition(0,playtime);
-  delay(1000);
-*/
   
 }
